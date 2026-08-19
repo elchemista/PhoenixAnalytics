33659749c2e8bdee5edf2b273747bfb16dbf2b60
Yuriy Zhar <darkyfelis@gmail.com>
2026-08-19 15:25:48 +0200

    refactor: credo warning and dialyzer cleanup

--- lib/phoenix_analytics/web/live/components/charts/device_chart.ex
+++ lib/phoenix_analytics/web/live/components/charts/device_chart.ex
@@ -3,12 +3,9 @@ defmodule PhoenixAnalytics.Web.Live.Components.DeviceChart do
 
   use PhoenixAnalytics.Web, :live_component
 
-  alias PhoenixAnalytics.Services.Cache
-  alias PhoenixAnalytics.Services.Telemetry
+  alias PhoenixAnalytics.Web.Data
 
-  alias PhoenixAnalytics.Queries.Analytics
-
-  @impl true
+  @impl Phoenix.LiveComponent
   def render(assigns) do
     ~H"""
     <div>
@@ -22,58 +19,19 @@ defmodule PhoenixAnalytics.Web.Live.Components.DeviceChart do
     """
   end
 
-  @impl true
+  @impl Phoenix.LiveComponent
   def update(assigns, socket) do
-    date_range = assigns.date_range
-
-    # Check if date_range has changed
-    should_refresh = socket.assigns[:date_range] != date_range
-
+    %{date_range: date_range} = assigns
+    refresh? = socket.assigns[:date_range] != date_range
     socket = assign(socket, assigns)
 
-    if should_refresh do
+    if refresh? do
       {:ok,
        assign_async(socket, :chart_data, fn ->
-         {:ok, %{chart_data: chart_data(date_range)}}
+         {:ok, %{chart_data: Data.chart(:devices, date_range)}}
        end)}
     else
       {:ok, socket}
-    end
-  end
-
-  defp chart_data(%{from: from, to: to} = _date_range) do
-    cache_key = "devices_usage:#{from}:#{to}"
-
-    {_, value} = Cache.fetch(cache_key, fn -> fetch_data(from, to) end)
-
-    # Handle Cachex.Error structs that can't be JSON encoded
-    case value do
-      %Cachex.Error{} ->
-        []
-
-      _ ->
-        value
-    end
-  end
-
-  defp fetch_data(from, to) do
-    query = Analytics.devices_usage(from, to)
-    repo = PhoenixAnalytics.Config.get_repo()
-    result = repo.all(query)
-
-    case result do
-      [] ->
-        []
-
-      {:error, reason} ->
-        Telemetry.log_error(:fetch_data, reason)
-        []
-
-      _ ->
-        # Transform data to match frontend expectations
-        Enum.map(result, fn %{device: device, count: count} ->
-          %{"device" => device, "visits" => count}
-        end)
     end
   end
 end
